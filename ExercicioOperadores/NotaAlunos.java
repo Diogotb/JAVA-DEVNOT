264aad0df302675a9ff64dbd0a8cafa6f7941595
Diogotb <digogo@gmail.com>
2023-02-16 21:58:44 -0300

    16/02/23 final aula

---
+++ ExercicioOperadores/NotaAlunos.java
@@ -0,0 +1,12 @@
+package ExercicioOperadores;
+
+import java.util.Scanner;
+
+public class NotaAlunos {
+    Scanner sc = new Scanner(System.in);
+    public void notaFrequencia() {
+        System.out.println("Informe a Nota 1 do aluno:");
+        int nota1 = sc.nextInt();
+
+    }
+}
